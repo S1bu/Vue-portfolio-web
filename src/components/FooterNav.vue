d3cabbb3981f145ce8aa6e514105aeb1bf8da041
S1bu <Sibusisodyan@gmail.com>
2023-07-01 12:53:24 +0200

    footer done

---
+++ src/components/FooterNav.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <h3>Follow me</h3>
+        <div class="link">
+            <a href="http://www.linkedin.com/in/sibusiso-dyan-0004" target="_blank"><i class="bi bi-linkedin"></i></a> <!--linked in link-->
+            <a href="https://github.com/S1bu" target="_blank"><i class="bi bi-github"></i></a> <!--github-->
+        </div>
+       <p>Sibusiso Dyan  Copyright &copy; {{ Year }} <!--current year with using javascript--></p>
+    </div>
+</template>
+
+<script>
+       
+    export default {
+        data(){
+            return{
+                Year: new Date().getUTCFullYear() //getting the current year via javascript
+            }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
