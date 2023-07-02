c84ac17eb1f7bad3eab6d0b85c62c5cd8e9eb2f9
Sibusiso <Sibusisodyan@gmail.com>
2023-07-02 16:19:10 +0200

    data is not showing yet

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 import { createStore } from 'vuex'
-
+const myInfoLink = "https://s1bu.github.io/portfolio_server/data/Portfolio.json"
 export default createStore({
   state: {
+    Testimonials:null,
   },
   getters: {
   },
   mutations: {
+    setTestimonials(state, Testimonials){
+      state.Testimonials = Testimonials
+    },
   },
   actions: {
+    async fetchTestimonials(data){
+      try{
+        let res = await fetch(myInfoLink);
+        let { Testimonials } = await res.json()
+       data.commit('setTestimonials' , Testimonials )
+      }
+      catch(e){
+        console.log(e.message);
+      }
+    }
   },
   modules: {
   }
--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,15 +1,28 @@
 <template>
-    <div>
-        <h1>Testimonials</h1>
+    <div class="container">
+        <div class="card" v-for="testimonial in Testimonials" style="width: 18rem;" :key="testimonial.id">
+            <img :src="testimonial.image" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">{{ testimonial.fullname}} {{ testimonial.Relation }}</h5>
+              <p class="card-text">{{ testimonial.Description }}</p>
+            </div>
+          </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+     computed:{
+         Testimonials(){
+            return this.$store.state.Testimonials
+         }
+     },
+     mounted(){
+        this.$store.dispatch(' fetchTestimonials')
+     }
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style  scoped>
+ 
 </style>
