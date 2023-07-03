979bcb974b4a157b0d0b5349d5513d8f53ccd6de
S1bu <Sibusisodyan@gmail.com>
2023-07-03 14:19:54 +0200

    almost done with project

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,15 +1,67 @@
 <template>
-    <div>
-        <h1>Project page</h1>
+    <div class="project">
+        <div class="container">
+            <div class="card" v-for="project in project" style="width: 28rem;" :key="project.id">
+                <img :src="project.image" class="card-img-top" alt="project.project">
+                <div class="card-body">
+                  <h5 class="card-title">{{ project.project }}</h5>
+                  <p class="card-text">{{ project.Description }}</p>
+                  <div class="row">
+                    <div class="col">
+                        <a :href="project.codeUrl" class="btn btn-primary" target="_blank"><i class="bi bi-code"></i></a>
+                      </div>
+                      <div class="col">
+                        <a :href="project.showUrl" class="btn btn-primary" target="_blank"><i class="bi bi-laptop"></i></a>
+                      </div>
+                  </div>
+                </div>
+              </div>
+        </div>
     </div>
+    
 </template>
 
 <script>
-    export default {
-        
+   export default {
+    computed: {
+        project(){
+            return this.$store.state.projects
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProjects')
     }
+}
 </script>
 
 <style scoped>
+.container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+     
+}
+.container img{
+    object-fit:contain;
+    aspect-ratio: 3/2;
+}
+.project{
+    background-color: midnightblue;
+    background-size: cover ;
+}
+.card {
+    margin: 5px;
+    text-align: center;
+    backdrop-filter: blur(6px) saturate(165%);
+    -webkit-backdrop-filter: blur(6px) saturate(165%);
+    background-color: rgba(10, 18, 33, 0.91);
+    border-radius: 12px;
+    border: 1px solid rgba(255, 255, 255, 0.125);
+    color: white;
+  transition: 1s;
+}
+i{
+    padding: 10px;
+}
+
 
 </style>
