a72b70f66aa600d8756351fc0d77655899ae357b
S1bu <Sibusisodyan@gmail.com>
2023-07-03 12:24:37 +0200

    dont testimonial

--- src/views/TestimonialView.vue
+++ src/views/TestimonialView.vue
@@ -1,28 +1,69 @@
-<template>
-    <div class="container">
-        <div class="card" v-for="testimonial in testimonials" style="width: 18rem;" :key="testimonial.id">
-            <img :src="testimonial.image" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title">{{ testimonial.fullname}} {{ testimonial.Relation }}</h5>
-              <p class="card-text">{{ testimonial.Description }}</p>
+<template >
+    <div class="testimonial">
+        <div class="container">
+            <div class="card" v-for="testimonial in testimonials" style="width: 28rem;" :key="testimonial.id">
+                <div class="image">
+                    <img :src="testimonial.image" class="card-img-top" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ testimonial.fullname }} ({{ testimonial.Relation }})</h5>
+                    <p class="card-text">{{ testimonial.Description }}</p>
+                </div>
             </div>
-          </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-     computed:{
-         testimonials(){
+export default {
+    computed: {
+        testimonials() {
             return this.$store.state.testimonials
-         }
-     },
-     mounted(){
+        }
+    },
+    mounted() {
         this.$store.dispatch('fetchTestimonials')
-     }
     }
+}
 </script>
 
 <style  scoped>
- 
+
+.testimonial{
+   background-image: url('https://wallpapercave.com/wp/wp6616042.jpg');
+   background-size: cover ;
+}
+.container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+     
+}
+
+.container img {
+    width: 200px;
+    height: 200px; 
+    object-fit:contain;
+   aspect-ratio: 3/5;
+   margin-top: 5px;
+
+}
+
+.card {
+    margin: 5px;
+    text-align: center;
+    backdrop-filter: blur(6px) saturate(165%);
+    -webkit-backdrop-filter: blur(6px) saturate(165%);
+    background-color: rgba(10, 18, 33, 0.62);
+    border-radius: 12px;
+    border: 1px solid rgba(255, 255, 255, 0.125);
+    color: white;
+  transition: 1s;
+}
+
+
+ .card:hover{
+    border: 4px solid white;
+   /* animation: tada; /* referring directly to the animation's @keyframe declaration */
+    /*animation-duration: 5s; /* don't forget to set a duration! */
+ }
 </style>
