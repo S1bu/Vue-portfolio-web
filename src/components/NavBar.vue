04fb7178a53ee177239172f87a8fa3d87407682a
S1bu <74048211+S1bu@users.noreply.github.com>
2023-07-05 14:16:18 +0200

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,25 +5,27 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <router-link to="/">Home</router-link>
+                <router-link to="/">
+                  <i class="bi bi-house"></i> Home</router-link>
               </li>
               <li class="nav-item">
-                <router-link to="/about">About</router-link>
+                <router-link to="/about">
+                  <i class="bi bi-person-circle"></i> About </router-link>
               </li>
               <li class="nav-item">
-                <router-link to="/resume">Resume</router-link>
+                <router-link to="/resume"><i class="bi bi-briefcase"></i> Resume</router-link>
               </li>
               <li class="nav-item">
-                <router-link to="/project">Project</router-link>
+                <router-link to="/project"><i class="bi bi-laptop"></i> Project</router-link>
               </li>
               <li class="nav-item">
-                <router-link to="/testimonials">Testimonials</router-link>
+                <router-link to="/testimonials"><i class="bi bi-chat-dots"></i> Testimonials</router-link>
               </li>
               <li class="nav-item">
-                <router-link to="/contact">Contact</router-link>
+                <router-link to="/contact"> <i class="bi bi-telephone"></i> Contact</router-link>
               </li>
             </ul>
           </div>
@@ -39,5 +41,5 @@
 </script>
 
 <style scoped>
- 
+
 </style>
